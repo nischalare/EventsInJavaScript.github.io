32d5a22cb1f974fd3b7298dae4ba85b909840317
Nischal <nischal.aremanda1432@gmail.com>
2022-01-07 17:45:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById(
+      "demo").innerHTML = "A JavaScript Event is an action or occurrence which happens in the system you are programming, which the system tells you about so your code can react to them. ";
+}
+
+
